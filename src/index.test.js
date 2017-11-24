1d3333a2addab46f1104ec40db1b313127e4492d
MoonTahoe <alex@moonhighway.com>
2017-11-24 12:07:22 -0800

    Completes suggestion function

--- src/index.test.js
+++ src/index.test.js
@@ -2,29 +2,42 @@ import { suggest, group } from './'
 
 describe("suggest()", () => {
 
-    it("SUCCESS - groups of 12")
+    it("SUCCESS - groups of 12", () => {
+        const result = suggest(12)
+        expect(result).toEqual([
+            [3, 4],
+            [4, 3],
+            [6, 2],
+            [2, 6]
+        ])
+    })
 
-    it("SUCCESS - groups of 13")
+    it("SUCCESS - groups of 13", () => {
+        const result = suggest(13)
+        expect(result).toEqual(null)
+    })
 
-    it("SUCCESS - groups of 50")
+    it("SUCCESS - groups of 42", () => {
+        const result = suggest(42)
+        expect(result).toEqual([
+            [7, 6],
+            [6, 7],
+            [3, 14],
+            [14, 3],
+            [21, 2],
+            [2, 21]
+        ])
+    })
 
-    it("SUCCESS - groups of 51")
-
-    it("SUCCESS - groups of 42")
-
-    it("SUCCESS - groups of 2003")
-
-    it("SUCCESS - groups of 23")
-
-    it("SUCCESS - groups of 4")
-
-    it("SUCCESS - groups of 7")
-
-    it("ERROR - groups of 1")
+    it("ERROR - groups of 1", () => {
+        const result = suggest(1)
+        expect(result).toEqual(null)
+    })
     
-    it("ERROR - groups of 2")
-
-    it("ERROR - groups of 3")
+    it("ERROR - groups of 2", () => {
+        const result = suggest(2)
+        expect(result).toEqual(null)
+    })
 
 })
 
--- src/suggest.js
+++ src/suggest.js
@@ -1,1 +1,50 @@
-export const suggest = f => f
+import { compose } from 'ramda'
+
+const sizeOrLength = val => 
+    Array.isArray(val) ? arr.length : val
+
+const setStartValues = size => 
+    size % 2 === 0 ? 
+        { start: 2, interval: 1, size } : 
+        { start: 3, interval: 2, size }
+
+const suggestion = ({start, interval, size, result=[] }, fn) => 
+    fn({
+        start: start+interval,
+        interval,
+        size,
+        result: (size%start !== 0) ? 
+            result : 
+            [...result, start]
+    })
+  
+const buildSuggestions = ({start,interval,size,result=[]}) => 
+    start<=Math.floor(size/2) ? 
+        suggestion({start,interval,size,result}, buildSuggestions) : 
+        result
+
+const appendResultRows = (first, last, result) => [
+    ...result,
+    [first, last],
+    [last, first]
+]
+
+const createGroups = ([first, ...rest], result=[]) => {
+    const [ last, ...remaining ] = rest.reverse()
+    return !remaining.length ? 
+        appendResultRows(first, last, result).reverse() :
+        createGroups(
+            remaining, 
+            appendResultRows(first, last, result)
+        ) 
+}
+
+const sendResult = (suggestions) => 
+    !suggestions.length ? null : createGroups(suggestions) 
+
+export const suggest = size => compose(
+    sendResult,
+    buildSuggestions,
+    setStartValues,
+    sizeOrLength
+)(size)
